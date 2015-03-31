15de8d9c71e797b02f56749adf2bc13a98f6c5b8
Alexander Pinkerton <Alexpinkerton88@gmail.com>
2015-03-30 21:19:17 -0400

    Added argument support, Cleaned up Code

--- src/com/alexanderpinkerton/Main.java
+++ src/com/alexanderpinkerton/Main.java
@@ -3,23 +3,31 @@ package com.alexanderpinkerton;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
 
         StringComparisonUtility utility = new StringComparisonUtility();
+        //utility.setDebug(true);
 
-        //Example One
+        //System.out.println(utility.findLCS(args[1],args[0]));
+        //System.out.println(utility.computeEditDistance(args[1],args[0]));
+
+
+        //Example One       0.842105        a g t g t a c t
         //System.out.println(utility.findLCS("agttgtagct","agtgctact"));
-        //utility.computeEditDistance("agttgtagct","agtgctact");
+        //System.out.println(utility.computeEditDistance("agttgtagct","agtgctact"));
 
          /*Example Two      0.444444        n c a r n a     n c a o n a     */
         //System.out.println(utility.findLCS("ncaatournament","northcarolina"));
-        //utility.computeEditDistance("ncaatournament","northcarolina");
+        //System.out.println(utility.computeEditDistance("ncaatournament","northcarolina"));
+
+         /*Example Three    0.666667    a l g o r i t h m s r o c k s i l o v e t h i s c l a s s */
+        System.out.println(utility.findLCS("zalgoxrithmsrxocksxzilozxxvethzisclzaxzzsxs", "qalgoriythmsqrowwcksqqwqilowvqethiqsclqawssw"));
+        System.out.println(utility.computeEditDistance("zalgoxrithmsrxocksxzilozxxvethzisclzaxzzsxs","qalgoriythmsqrowwcksqqwqilowvqethiqsclqawssw"));
 
 
 
         //Document Example
-        System.out.println(utility.findLCS("agtacgtcat","gtatcgtat"));
-        utility.computeEditDistance("agtacgtcat","gtatcgtat");
+        //System.out.println(utility.findLCS("agtacgtcat","gtatcgtat"));
+        //utility.computeEditDistance("agtacgtcat","gtatcgtat");
 
     }
 
--- src/com/alexanderpinkerton/StringComparisonUtility.java
+++ src/com/alexanderpinkerton/StringComparisonUtility.java
@@ -4,30 +4,40 @@ import java.util.Collections;
 import java.util.Stack;
 
 /**
- * Created by Ace on 3/26/15.
- */
+ *
+ * Created by Alexander Pinkerton on 3/26/15.
+ *
+ **/
+
 public class StringComparisonUtility {
 
+    private boolean debug = false;
 
     public float computeEditDistance(String x, String y){
-
+        /*
+        This function will calculate the normalized edit distance between two strings via
+        dynamic programming and linear memory allocation. Only two rows of the table are needed to
+        obtain the final result.
+     */
         int columnCount = x.length()+1;
         int rowCount = y.length()+1;
 
         int[] backwardRow = new int[columnCount];
         int[] forwardRow = new int[columnCount];
+        int[] temp;
 
         //Populate the first row.
         for(int i=0;i<columnCount;i++){
             backwardRow[i] = i;
         }
 
+        //Start at the forward row since the backward row is given.
         for(int rowIndex = 1; rowIndex < rowCount; rowIndex++){
             //Insert the correct value for the first element on string y.
             forwardRow[0] = rowIndex;
 
+            //Start at the second column since the first column is given by the previous statement.
             for(int columnIndex = 1; columnIndex < columnCount; columnIndex++){
-
                     if(x.charAt(columnIndex-1) == y.charAt(rowIndex-1)){
                      // If the two symbols are equal, we copy the diagonal top-left value to the new cell.
                         forwardRow[columnIndex] = backwardRow[columnIndex - 1];
@@ -40,35 +50,42 @@ public class StringComparisonUtility {
                         forwardRow[columnIndex] = (top > left) ? ++left : ++top;
                     }
             }
-
-            if(rowIndex==1){
-                for(int i=0;i<columnCount;i++){
-                    System.out.print(backwardRow[i] + " ");
+            //=================DEBUG OUTPUT===================
+            if(debug) {
+                if (rowIndex == 1) {
+                    for (int i = 0; i < columnCount; i++) {
+                        System.out.print(backwardRow[i] + " ");
+                    }
+                    System.out.println("-----------B-------------");
                 }
-                System.out.println("-----------B-------------");
+                for (int i = 0; i < columnCount; i++) {
+                    System.out.print(forwardRow[i] + " ");
+                }
+                System.out.println("-----------F------------");
             }
-            for(int i=0;i<columnCount;i++){
-                System.out.print(forwardRow[i] + " ");
-            }
-            System.out.println("-----------F------------");
+            //=================DEBUG OUTPUT===================
 
-            int[] temp = backwardRow;
+            //Swap the two rows for Linear Memory.
+            temp = backwardRow;
             backwardRow = forwardRow;
             forwardRow = temp;
         }
 
+        //(S1+S2-NED)/(S1+S2) = Normalized Edit Distance
+        float normalizedEditDistance = (x.length()+y.length()-forwardRow[columnCount-1]) / (float)(x.length() + y.length());
 
-
-        return 69;
+        return normalizedEditDistance;
     }
 
 
 
 
-
-
-
     public int[][] computeEditDistanceTable(String x, String y){
+        /*
+            This function will calculate and build the entire table to be used for
+            the Least Common Substring backtrace. This does not use linear memory as
+            the entire table is stored.
+         */
 
         //Initialize the table to the size based on the input.
         //Add one to create a blank corner.
@@ -78,7 +95,7 @@ public class StringComparisonUtility {
         int[][] table = new int[rowCount][columnCount];
 
 
-        //Initialize the table =============
+        //============= Initialize the table =============
         //Downwards
         for(int i=0; i<columnCount;i++){
             table[0][i] = i;
@@ -88,11 +105,9 @@ public class StringComparisonUtility {
         for(int i=0; i<rowCount;i++){
             table[i][0] = i;
         }
-        //Initialize the table =============
+        //================================================
 
-
-
-        //MAIN TABLE GENERATION
+        //Loop through every cell to assign a value. Skip the first rows since they are complete.
         for(int rowIndex = 1; rowIndex < rowCount; rowIndex++){
 
             for(int columnIndex = 1; columnIndex < columnCount; columnIndex++){
@@ -108,33 +123,25 @@ public class StringComparisonUtility {
                     int left = table[rowIndex][columnIndex-1];
 
                     table[rowIndex][columnIndex] = (top > left) ? ++left : ++top;
-
                 }
-
             }
-
         }
-
-
         //================DEBUG OUTPUT================
-        System.out.print("  ");
-        for(int i=0;i<rowCount;i++){
-            System.out.print(x.charAt(i) + " ");
-        }
-
-        System.out.println();
-
-        for(int i=0;i<rowCount;i++){
-            for(int j=0;j<columnCount;j++){
-                //if(j==0 && i<y.length() && i>0){System.out.print(y.charAt(i-1) + " ");}else if(i==0 && j==0){System.out.print("  ");}
-                System.out.print(table[i][j] + " ");
+        if(debug) {
+            System.out.print("  ");
+            for (int i = 0; i < rowCount; i++) {
+                System.out.print(x.charAt(i) + " ");
             }
             System.out.println();
+            for (int i = 0; i < rowCount; i++) {
+                for (int j = 0; j < columnCount; j++) {
+                    //if(j==0 && i<y.length() && i>0){System.out.print(y.charAt(i-1) + " ");}else if(i==0 && j==0){System.out.print("  ");}
+                    System.out.print(table[i][j] + " ");
+                }
+                System.out.println();
+            }
         }
-
-        //================DEBUG OUTPUT================
-
-
+        //=============================================
         return table;
     }
 
@@ -142,13 +149,18 @@ public class StringComparisonUtility {
 
 
     public String findLCS(String x, String y){
-
-        //TODO Use linear memory.
+        /*
+            This function will use a complete edit distance table to obtain the
+            Longest Common Substring. Starting from the bottom right cell of the table,
+            the algorithm will move backwards through the data to find the LCS.
+         */
 
         int[][] table = computeEditDistanceTable(x,y);
 
         Stack<Character> substring = new Stack<Character>();
 
+        //Starting from the bottom-right cell, backtrace diagonally backwards until the end of the table is reached.
+        //The control flow statements will modify the values and reroute the backtrace loop to find the LCS.
         for(int columnIndex = x.length(), rowIndex = y.length(); columnIndex > 0 && rowIndex > 0; columnIndex--,rowIndex--){
 
             int top = table[rowIndex-1][columnIndex];
@@ -157,45 +169,35 @@ public class StringComparisonUtility {
             //If the symbols are equal, push the symbol on to a stack and move to the diagonal top-left neighbor.
             if(x.charAt(columnIndex-1) == y.charAt(rowIndex-1)){
                 substring.push(x.charAt(columnIndex-1));
-                System.out.println("Diagonal Movement");
+                //System.out.println("Diagonal Movement");
             }else if(top==left){
                 rowIndex++;
-                System.out.println("Left Movement");
+                //System.out.println("Left Movement");
             }else if(top>left){
                 rowIndex++;
-                System.out.println("Left Movement");
+                //System.out.println("Left Movement");
             }else{
                 columnIndex++;
-                System.out.println("Upward Movement");
+                //System.out.println("Upward Movement");
             }
-
         }
 
         Collections.reverse(substring);
 
-        return substring.toString();
+        StringBuilder b = new StringBuilder();
+        for (char value : substring) {
+            b.append(value);
+        }
+
+        return b.toString();
     }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public void setDebug(boolean debug){
+        this.debug = debug;
+    }
 
 
 }
